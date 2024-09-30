b2dbdea77926336a8d642fd5e6003f47dd955eff
Rubben <147255793+Ryxena@users.noreply.github.com>
2024-09-30 12:11:19 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Helper\ApiResponse;
-use App\Models\user;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
