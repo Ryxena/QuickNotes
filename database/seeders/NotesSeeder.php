55f5f691e5eb67941c8a8266607926879d790766
Ryxena <Ryxena@users.noreply.github.com>
2024-10-21 12:37:54 +0000

    style: Fix coding style issues

--- database/seeders/NotesSeeder.php
+++ database/seeders/NotesSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\Category;
 use App\Models\Notes;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class NotesSeeder extends Seeder
@@ -31,29 +30,29 @@ class NotesSeeder extends Seeder
                 'content' => 'This is a general note by Kira.',
                 'status' => 'regular',
                 'users_id' => $userKira->id,
-                'category_ids' => [$categoryGeneral->id]
+                'category_ids' => [$categoryGeneral->id],
             ],
             [
                 'title' => 'Jotaro\'s Work Note',
                 'content' => 'This is a work-related note by Jotaro.',
                 'status' => 'regular',
                 'users_id' => $userJotaro->id,
-                'category_ids' => [$categoryWork->id]
+                'category_ids' => [$categoryWork->id],
             ],
             [
                 'title' => 'Dio\'s Personal Note',
                 'content' => 'This is a personal note by Dio.',
                 'status' => 'favorite',
                 'users_id' => $userDio->id,
-                'category_ids' => [$categoryPersonal->id]
+                'category_ids' => [$categoryPersonal->id],
             ],
             [
                 'title' => 'Dio\'s Over Heavens Note',
                 'content' => 'This is a personal note by Dio.',
                 'status' => 'favorite',
                 'users_id' => $userDio->id,
-                'category_ids' => null
-            ]
+                'category_ids' => null,
+            ],
         ];
 
         foreach ($notes as $noteData) {
@@ -62,7 +61,7 @@ class NotesSeeder extends Seeder
                 'title' => $noteData['title'],
                 'content' => $noteData['content'],
                 'status' => $noteData['status'],
-                'users_id' => $noteData['users_id']
+                'users_id' => $noteData['users_id'],
             ]);
 
             // Sync dengan kategori
